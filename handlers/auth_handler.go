c4c1f3b960cd82fed2c41d2fd9cb6932ffa3f440
Kelvin J. Pardo <jrpardo92@gmail.com>
2024-01-16 22:42:57 -0500

    Encriptacion de contraseña  al registrar y utilizacion de JWT para el login.

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	// Otras importaciones necesarias
 	"database/sql"
+	"log"
 	"my-football-app/config"
 	"my-football-app/models"
 	"net/http"
@@ -27,29 +28,35 @@ func LoginHandler(c echo.Context) error {
 
 	db, err := config.GetDBConnection()
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError,
-			"Error de conexión a la base de datos")
+		return c.JSON(http.StatusInternalServerError, "Error de conexión a la base de datos")
 	}
 	defer db.Close()
 
-	// Buscar usuario por correo electrónico
-	var hashedPassword string
-	err = db.QueryRow("SELECT Contraseña FROM Usuarios WHERE CorreoElectronico = $1", loginReq.CorreoElectronico).Scan(&hashedPassword)
+	var user models.User
+	err = db.QueryRow("SELECT UsuarioID, Contraseña FROM Usuarios WHERE CorreoElectronico = $1", loginReq.CorreoElectronico).Scan(&user.UsuarioID, &user.Contraseña)
+	//comprobar correo
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return c.JSON(http.StatusUnauthorized, "Correo electrónico incorrecto")
+			return c.JSON(http.StatusUnauthorized, "Correo electrónico")
 		}
 		return c.JSON(http.StatusInternalServerError, "Error al consultar la base de datos")
 	}
-
-	// Comprobar la contraseña hasheada
-	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(loginReq.Contraseña)); err != nil {
-		return c.JSON(http.StatusUnauthorized, "contraseña incorrecta")
+	// Comprobar la contraseña
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Contraseña), []byte(loginReq.Contraseña)); err != nil {
+		return c.JSON(http.StatusUnauthorized, " contraseña incorrectos")
 	}
 
-	// Lógica para el inicio de sesión exitoso (puede incluir generación de token JWT, etc.)
-	// ...
-	return c.JSON(http.StatusOK, "Inicio de sesión exitoso")
+	// Generar JWT
+	tokenString, err := generateJWT(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Error al generar el token")
+	}
+
+	// Devolver el token en la respuesta
+	return c.JSON(http.StatusOK, echo.Map{
+		"mensaje": "Inicio de sesión exitoso",
+		"token":   tokenString,
+	})
 }
 
 // generateJWT genera un nuevo token JWT para un usuario
@@ -63,10 +70,11 @@ func generateJWT(user models.User) (string, error) {
 	// Puedes agregar más claims aquí si lo deseas
 
 	// Firmar y obtener el token en formato de cadena
-	tokenString, err := token.SignedString([]byte("tuSecretoAquí")) // Usa una clave secreta adecuada
+	tokenString, err := token.SignedString([]byte("aB3!f#9ZpQ8rS*7tVx4wC&1dE@5gH+6j")) // Utiliza una clave secreta adecuada y segura
 	if err != nil {
+		log.Printf("Error al generar el token JWT: %v", err)
 		return "", err
 	}
-
+	log.Printf("Token generado: %s", tokenString)
 	return tokenString, nil
 }
